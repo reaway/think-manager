bc9c5119726ea3b2d5de04be7c8657ff160e7840
reaway <reaway@foxmail.com>
2026-03-09 12:22:38 +0800

    init

---
+++ src/Manager.php
@@ -0,0 +1,201 @@
+<?php
+declare (strict_types=1);
+
+namespace Think\Component\Manager;
+
+use InvalidArgumentException;
+use think\helper\Str;
+use Think\Component\Container\Container;
+
+abstract class Manager
+{
+    /** @var Container */
+    protected $container;
+
+    /**
+     * 驱动
+     * @var array
+     */
+    protected $drivers = [];
+
+    /**
+     * 驱动的命名空间
+     * @var string
+     */
+    protected $namespace = null;
+
+    /**
+     * 配置参数
+     * @var array
+     */
+    protected $config = [];
+
+    public function __construct(array $config = [])
+    {
+        $this->container = Container::getInstance();
+        $this->config = array_merge($this->config, $config);
+    }
+
+    /**
+     * 设置配置
+     * @param array $config 配置参数
+     * @return void
+     */
+    public function setConfig(array $config): void
+    {
+        $this->config = array_merge($this->config, $config);
+    }
+
+    /**
+     * 获取配置
+     * @param null|string $name 名称
+     * @param mixed $default 默认值
+     * @return mixed
+     */
+    public function getConfig(?string $name = null, $default = null)
+    {
+        if (!is_null($name)) {
+            return $this->config[$name] ?? $default;
+        }
+
+        return $this->config;
+    }
+
+    /**
+     * 获取驱动实例
+     * @param null|string $name
+     * @return mixed
+     */
+    protected function driver(?string $name = null)
+    {
+        $name = $name ?: $this->getDefaultDriver();
+
+        if (is_null($name)) {
+            throw new InvalidArgumentException(sprintf(
+                'Unable to resolve NULL driver for [%s].',
+                static::class
+            ));
+        }
+
+        return $this->drivers[$name] = $this->getDriver($name);
+    }
+
+    /**
+     * 获取驱动实例
+     * @param string $name
+     * @return mixed
+     */
+    protected function getDriver(string $name)
+    {
+        return $this->drivers[$name] ?? $this->createDriver($name);
+    }
+
+    /**
+     * 获取驱动类型
+     * @param string $name
+     * @return mixed
+     */
+    protected function resolveType(string $name)
+    {
+        return $name;
+    }
+
+    /**
+     * 获取驱动配置
+     * @param string $name
+     * @return mixed
+     */
+    protected function resolveConfig(string $name)
+    {
+        return $name;
+    }
+
+    /**
+     * 获取驱动类
+     * @param string $type
+     * @return string
+     */
+    protected function resolveClass(string $type): string
+    {
+        if ($this->namespace || str_contains($type, '\\')) {
+            $class = str_contains($type, '\\') ? $type : $this->namespace . Str::studly($type);
+
+            if (class_exists($class)) {
+                return $class;
+            }
+        }
+
+        throw new InvalidArgumentException("Driver [$type] not supported.");
+    }
+
+    /**
+     * 获取驱动参数
+     * @param $name
+     * @return array
+     */
+    protected function resolveParams($name): array
+    {
+        $config = $this->resolveConfig($name);
+        return [$config];
+    }
+
+    /**
+     * 创建驱动
+     *
+     * @param string $name
+     * @return mixed
+     *
+     */
+    protected function createDriver(string $name)
+    {
+        $type = $this->resolveType($name);
+
+        $method = 'create' . Str::studly($type) . 'Driver';
+
+        $params = $this->resolveParams($name);
+
+        if (method_exists($this, $method)) {
+            return $this->$method(...$params);
+        }
+
+        $class = $this->resolveClass($type);
+
+        return $this->container->invokeClass($class, $params);
+    }
+
+    /**
+     * 移除一个驱动实例
+     *
+     * @param array|string|null $name
+     * @return $this
+     */
+    public function forgetDriver($name = null)
+    {
+        $name = $name ?? $this->getDefaultDriver();
+
+        foreach ((array)$name as $cacheName) {
+            if (isset($this->drivers[$cacheName])) {
+                unset($this->drivers[$cacheName]);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * 默认驱动
+     * @return string|null
+     */
+    abstract public function getDefaultDriver();
+
+    /**
+     * 动态调用
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return $this->driver()->$method(...$parameters);
+    }
+}
